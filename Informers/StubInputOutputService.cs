56bdaa814b2450ce8b229daffd3cb1912620450b
Dmitry Fedosov <fedosovdn95@gmail.com>
2021-03-18 17:35:57 +0300

    IO сервис, когда нужно отключить интерактив и загружать состояние юзера из файла

---
+++ Informers/StubInputOutputService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informers
+{
+    public class StubInputOutputService : IInputOutputService
+    {
+        public string UserName { get; set; }
+
+        public Task<EAgreementAnswer> GetAgreementAnswer(string answerText)
+        {
+            return Task.FromResult(EAgreementAnswer.Yes);
+        }
+
+        public Task<string> GetMessage()
+        {
+            return Task.FromResult(UserName);
+        }
+
+        public Task<bool> OutputMessageAsync(string message)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
